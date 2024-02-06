aec7dec66d9f2ce3d38352e6099d44041918d1b8
SheetalShankarr <95228558+SheetalShankarr@users.noreply.github.com>
2024-02-06 16:49:08 +0530

    first commit

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,17 @@
+import { Box } from '@mui/material';
+import Header from '../../components/header'
+
+const Dashboard = () => {
+
+  return(
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subTitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+
+  )
+
+}
+
+export default Dashboard
